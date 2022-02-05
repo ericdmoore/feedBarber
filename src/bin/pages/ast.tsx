1c91e30ac5aaa6552223cc1ba50814d05073c1c3
Eric Moore <eric.d.moore@gmail.com>
2022-02-04 22:51:19 -0600

    think I got it

--- src/bin/pages/ast.tsx
+++ src/bin/pages/ast.tsx
@@ -3,7 +3,7 @@ import json from '../comps/responders/json.ts'
 import { h, Handler, jsx, serve } from 'https://deno.land/x/sift@0.4.3/mod.ts';
 
 export const astDisplay: Handler = async (req, param) => {
-    console.log('req.json(): \n', await req.json())
+    // console.log('req.json(): \n', await req.json())
     return json({method: req.method, 
         referrer: req.referrer, 
         hdrs: req.headers,
