386f84cda0175242b784e2e8ecf157919f432bc0
Eric Moore <eric.d.moore@gmail.com>
2022-02-04 22:40:46 -0600

    can i figuree out how to echo the request param to screen?

--- src/bin/pages/ast.tsx
+++ src/bin/pages/ast.tsx
@@ -3,6 +3,6 @@ import json from '../comps/responders/json.ts'
 import { h, Handler, jsx, serve } from 'https://deno.land/x/sift@0.4.3/mod.ts';
 
 export const astDisplay: Handler = async (req, param) => {
-    return json({req, param})
+    return json({req: JSON.stringify(req), request:req.toString(), param})
 }
 export default astDisplay
