402a2563766733ca48b5854d13b620242054232f
Eric Moore <eric.d.moore@gmail.com>
2022-02-04 22:44:34 -0600

    geting closer

--- src/bin/pages/ast.tsx
+++ src/bin/pages/ast.tsx
@@ -3,6 +3,7 @@ import json from '../comps/responders/json.ts'
 import { h, Handler, jsx, serve } from 'https://deno.land/x/sift@0.4.3/mod.ts';
 
 export const astDisplay: Handler = async (req, param) => {
-    return json({req: await req.json(), param})
+    console.log('req.json(): \n',await req.json())
+    return json({req: await req.text(), param})
 }
 export default astDisplay
