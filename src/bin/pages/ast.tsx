a07c8f7694a94c3c7f94c9dcdfd3289ad4dfe851
Eric Moore <eric.d.moore@gmail.com>
2022-02-08 14:08:14 -0600

    added top level _reflect key to reesponse

--- src/bin/pages/ast.tsx
+++ src/bin/pages/ast.tsx
@@ -4,13 +4,41 @@ import {fetchAndValidateIntoAST} from '../../lib/start.ts'
 import { Handler, json } from 'https://deno.land/x/sift@0.4.3/mod.ts';
 
 
+const buildSearchParams = (req: Request)=>{
+    const searchParams = {} as {[paramName:string]: string | string[] }
+    const sp = new URL(req.url).searchParams
+    sp.forEach((v,k,p)=>{
+        console.log({v,k,p})
+        if(k in searchParams){
+            if(Array.isArray(searchParams[k])){
+                searchParams[k] = [...searchParams[k], v]
+            }
+            else{
+                searchParams[k] = [searchParams[k], v] as string[]
+            }
+        }else{
+            searchParams[k] = v
+        }
+    })
+    return searchParams
+}
+
+
 export const urlAST = '/ast/*'
 export const echoAST: Handler = async (req, param) => {
     if(param?.url){
         const r = await fetchAndValidateIntoAST(param?.url)
                         .catch(()=> null)
+        const searchParams = buildSearchParams(req)
         if(r){
-            return json(r)
+            return json({
+                _reflect: {
+                    '@self': req?.url,
+                    sourceURL: param?.url,
+                    searchParams,
+                },
+                ...r,
+            })
         }else{
             return json({
                 method: req.method, 
