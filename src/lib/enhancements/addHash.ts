74bf64b97e09b5779cc20216a0e3e6db7700a7b3
Eric Moore <eric.d.moore@gmail.com>
2022-02-21 22:57:20 -0600

    updates for hashes / CID / and mod.ts

---
+++ src/lib/enhancements/addHash.ts
@@ -0,0 +1,33 @@
+import type {PromiseOr, ASTComputable} from '../../types.ts'
+import { computableToJson } from '../parsers/ast.ts'
+import {cidStr} from '../analysis/calcMultihash.ts'
+
+export const addHash = (_i:never)=> async (ast :PromiseOr<ASTComputable>):Promise<ASTComputable>=>{
+    const a = await computableToJson(ast)
+    
+    const itemHashes = await Promise.all(a.items.map(async (i) =>{
+        const content = i.content.text ?? i.content.markdown ?? i.content.html
+        return content ? await cidStr(content) : undefined   
+    }))
+
+    const hashedNullItems = (await Promise.all(itemHashes.map(async (i) => { 
+        return i ? i : cidStr('null')
+    }))).join('')
+
+    return {
+        ...a,
+        _meta:{
+            ...a._meta, _type: 'computable',
+            source:{ 
+                ...a._meta.source, 
+                hash: await cidStr(hashedNullItems)
+            }
+        },
+        item:{
+            next: async ()=>[],
+            list: a.items.map((item, i) =>{
+                return { ...item, source:{  ...(itemHashes[i] ? {hash: itemHashes[i]} : {} )  } }
+            })
+        }
+    }
+}
--- src/mod.ts
+++ src/mod.ts
@@ -7,8 +7,12 @@ export * as fromXml from 'https://deno.land/x/xml2js@1.0.0/mod.ts';
 export * as toXml from 'https://deno.land/x/js2xml@1.0.2/mod.ts';
 export * as DenoDom from 'https://deno.land/x/deno_dom@v0.1.21-alpha/deno-dom-wasm.ts';
 export * as mustache from 'https://deno.land/x/mustache@v0.3.0/mod.ts';
-export * as multiformat from 'https://cdn.skypack.dev/multiformats@9.6.4?dts'
-export * as minhash from 'https://cdn.skypack.dev/minhash@v0.0.9?dts'
+export * as multiformat from 'https://cdn.skypack.dev/multiformats@9.6.4?dts';
+
+// import { Minhash, LshIndex } from "https://cdn.skypack.dev/minhash@0.0.9?dts";
+// export { Minhash, LshIndex }
+import mh from 'https://cdn.skypack.dev/minhash@v0.0.9?dts'
+export { mh as minhash }
 
 // export * as vfile from 'https://cdn.skypack.dev/vfile?dts';
 // export * as unified from 'https://cdn.skypack.dev/unified@10?dts';
