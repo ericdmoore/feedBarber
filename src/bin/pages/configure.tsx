b9f09101f57621b7172f5a32132e6246e82a877b
Eric Moore <eric.d.moore@gmail.com>
2022-02-22 22:24:51 -0600

    configure

--- src/bin/pages/configure.tsx
+++ src/bin/pages/configure.tsx
@@ -3,7 +3,17 @@ import { h, Handler, jsx } from 'https://deno.land/x/sift@0.4.3/mod.ts';
 
 export const configure = (s = 'Configure Composition'): Handler =>
 	async (req, pathParam) => {
-		return jsx(h('h1', {}, s));
+		return jsx(
+			<div>
+				<h1>{s}</h1>
+				<p>Here we will</p>
+				<ul>
+					<li>Show all the functions to be used</li>
+					<li>Show all params setup for each function</li>
+					<li>Sync the URL with function(param) and order</li>
+				</ul>
+			</div>
+		);
 	};
 
 export default configure;
--- src/bin/pages/preview.tsx
+++ src/bin/pages/preview.tsx
@@ -4,7 +4,9 @@ import type { Handler } from 'https://deno.land/x/sift@0.4.3/mod.ts';
 import { h, jsx } from 'https://deno.land/x/sift@0.4.3/mod.ts';
 
 export const preview: Handler = async (req, pathParam) => {
-	return jsx(<h1>The Preview </h1>)
+	return jsx(
+		<h1>The Preview </h1>
+	)
 };
 
 export default preview;
