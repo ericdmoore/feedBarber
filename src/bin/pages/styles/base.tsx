81c189393ac3ac606d3c03a7fad53e265a954ae6
Eric <eric@mooore.cc>
2022-08-12 08:27:37 -0500

    styling system

--- src/bin/pages/styles/base.tsx
+++ src/bin/pages/styles/base.tsx
@@ -1,4 +1,10 @@
-import { setup } from "https://esm.sh/twind@0.16.16";
+/* 
+
+@Research: Does the style system export a sheet or presets?
+
+ */
+
+import { setup,   } from "https://esm.sh/twind@0.16.16";
 import { virtualSheet } from "https://esm.sh/twind@0.16.16/sheets";
 
 const sheet = virtualSheet();
@@ -12,3 +18,5 @@ setup({
   },
   sheet,
 });
+
+export default sheet
